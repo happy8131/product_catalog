7567071711fd1e673be919361dc8fcf5ff483125
iljoung oh <70251881+happy8131@users.noreply.github.com>
2026-01-05 03:27:06 +0900

    타입 추가 및 swr 설치

---
+++ types/auth.ts
@@ -0,0 +1,10 @@
+export interface AccountDTO {
+    email: string;
+    password?: string;
+    nickname: string;
+    role: string;
+    accessToken?: string; // 토큰은 로그인 후에만 존재하므로 선택적 속성으로 설정
+    refreshToken?: string;
+    joinDate: string; // @JsonFormat에 맞게 string으로 변환
+    modifiedDate: string; // @JsonFormat에 맞게 string으로 변환
+}
